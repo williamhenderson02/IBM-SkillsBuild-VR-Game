b574d4ecadeedcb57eefad1594365fa468603347
William Henderson <tgpz35@durham.ac.uk>
2023-03-10 11:29:40 +0000

    runs script without game object

--- IBM_Space_Escape/Assets/Scripts/db2.cs
+++ IBM_Space_Escape/Assets/Scripts/db2.cs
@@ -1,27 +1,40 @@
 using IBM.Data.DB2.Core;
 using System.Data;
+using IBM.Data.DB2;
+using UnityEngine;
 
-public class DB2Connector
+public class db2
 {
-    private string connectionString = "Database=<database_name>;UserID=<user_id>;Password=<password>;Server=<server_name>.databases.appdomain.cloud:50000/;";
+    private static string connectionString = "Database=Space-Escape-dB2;Hostname=bs2ipcul0apon0jufi80lite.db2.cloud.ibm.com;Port=31929;Protocol=TCPIP;UID=frx98001;PWD=rFKWq6qM3zKKP7pd;";
 
-    public void QueryDatabase(string query)
+    [RuntimeInitializeOnLoadMethod]
+    static void Start()
     {
+        // Create connection
         using (DB2Connection connection = new DB2Connection(connectionString))
         {
             connection.Open();
 
+            // Create a SQL command object
             using (DB2Command command = connection.CreateCommand())
             {
+                command.CommandText = "SELECT * FROM USERS";
                 command.CommandType = CommandType.Text;
-                command.CommandText = query;
 
+                // Execute the SQL command and read the results
                 using (DB2DataReader reader = command.ExecuteReader())
                 {
-                    // Handle the returned data
+                    while (reader.Read())
+                    {
+                        // Access column values using reader methods
+                        int id = reader.GetInt32(0);
+                        string username = reader.GetString(1);
+                        string password = reader.GetString(2);
+                    }
                 }
             }
 
+            // Close connection
             connection.Close();
         }
     }
