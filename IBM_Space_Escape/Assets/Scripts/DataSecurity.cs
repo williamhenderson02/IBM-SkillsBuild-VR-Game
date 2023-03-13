acfe967c7a354b80752b4736204141c390c6cdae
knightjam1 <joshball4444@gmail.com>
2023-03-13 13:02:13 +0000

    Add some quiz code

--- IBM_Space_Escape/Assets/Scripts/DataSecurity.cs
+++ IBM_Space_Escape/Assets/Scripts/DataSecurity.cs
@@ -6,7 +6,10 @@ using TMPro;
 
 public class DataSecurity : MonoBehaviour
 {
-    public TextMeshProUGUI responseText;
+    public TextMeshProUGUI aText1;
+    public TextMeshProUGUI aText2;
+    public TextMeshProUGUI aText3;
+    public TextMeshProUGUI aText4;
     public string[,] rows;
  
     public void NewResponse()
@@ -21,9 +24,13 @@ public class DataSecurity : MonoBehaviour
         string questionText = rows[qNo, 1];
 
         string option1 = rows[qNo, 2];
+        aText1.text = "Answer 1: " + option1;
         string option2 = rows[qNo, 3];
+        aText2.text = "Answer 2: " + option2;
         string option3 = rows[qNo, 4];
+        aText3.text = "Answer 3: " + option3;
         string option4 = rows[qNo, 5];
+        aText4.text = "Answer 4: " + option4;
     }
 
     public void Check(int num, int qNo)
@@ -33,9 +40,11 @@ public class DataSecurity : MonoBehaviour
         if (num == int.Parse(answer))
         {
             // do something
+            Debug.Log("");
         }
         else
         {
+            Debug.Log("");
             // incorrect
         }
     }
