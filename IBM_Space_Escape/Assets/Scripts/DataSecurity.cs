8dffb34c483ec7f5374783cf84cbaa3e08b34d60
William Henderson <tgpz35@durham.ac.uk>
2023-03-13 12:48:35 +0000

    started quiz script

--- IBM_Space_Escape/Assets/Scripts/DataSecurity.cs
+++ IBM_Space_Escape/Assets/Scripts/DataSecurity.cs
@@ -7,13 +7,37 @@ using TMPro;
 public class DataSecurity : MonoBehaviour
 {
     public TextMeshProUGUI responseText;
-
+    public string[,] rows;
+ 
     public void NewResponse()
     {
         Response r = DB2apiSecurity.GetNewResponse();
-        string[] columns = r.columns;
-        string[] rows = r.rows;
-        responseText.text = columns[0];
+        rows = r.rows;
+
+    }
+
+    public void Display(int qNo)
+    {
+        string questionText = rows[qNo, 1];
+
+        string option1 = rows[qNo, 2];
+        string option2 = rows[qNo, 3];
+        string option3 = rows[qNo, 4];
+        string option4 = rows[qNo, 5];
+    }
+
+    public void Check(int num, int qNo)
+    {
+        string answer = rows[qNo, 6];
+
+        if (num == int.Parse(answer))
+        {
+            // do something
+        }
+        else
+        {
+            // incorrect
+        }
     }
 }
 
